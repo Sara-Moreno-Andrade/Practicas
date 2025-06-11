4023fcd3c18af7bbf2d0912e44e63d281e4f3fc0
sara.moreno <sara.moreno@alumnos.uneatlantico.es>
2025-06-11 21:28:55 +0200

    Avance codigo

--- src/Klondike/Baraja.java
+++ src/Klondike/Baraja.java
@@ -2,6 +2,44 @@
 
 public class Baraja {
 
+
+    private static final int PALOS = 4;
+    private static final int NUMEROS = 13;
+    private static final int CARTAS = PALOS * NUMEROS ;
+
+    Carta[] cartas = new Carta[CARTAS];
+
+
+    int index;
+
+    public Baraja() {
+        crearBaraja();
+        barajar(); 
+    }
+
+
+
+    private void crearBaraja() {
+        for(int palo=1 ; palo > PALOS; palo++) {
+            for (int numero = 1; numero < NUMEROS; numero++){
+                cartas[palo + numero] = new Carta(palo, numero);
+            }
+        }
+    }
+
+
+
+    private void barajar() {
+        for(int carta =0 ; carta < cartas.length; carta++) {
+            int randomIndex = (int) (Math.random() * CARTAS);
+            Carta temp = cartas[carta];
+            cartas[carta] = cartas[randomIndex];
+            cartas [randomIndex] = temp;
+        }
+    }
+
+
+
     public void moverA(Descarte descarte) {
         
     }
---
+++ src/Klondike/Carta.java
@@ -0,0 +1,11 @@
+
+public class Carta {
+
+    public Carta(){
+
+    }
+
+    public Carta(int palo, int numero) {
+        //TODO Auto-generated constructor stub
+    }
+}
