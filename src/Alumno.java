10aecad4d030d58664c752276a9a5f09a368fb19
sara.moreno@alumnos.uneatlantico.es <sara.moreno@alumnos.uneatlantico.es>
2025-06-11 13:22:01 +0200

    Código y teoría inicial de métodos.

--- src/Alumno.java
+++ src/Alumno.java
@@ -4,5 +4,17 @@ public class Alumno{
     int id;
     String nombre;
     String apellido;
-    //Abstraemos las características de Alumnos
+    //Abstraemos las características de Alumnos (atributos).
+
+    public void mostrarNombre (){
+        System.out.println("Hola, soy un alumno y se decir mi nombre:" + nombre);
+    }
+
+    public void saberAprobado (double calificacion){
+        if(calificacion >= 6){
+            System.out.println("Aprobé la materia.");
+        }else{
+            System.out.println("Suspendí la materia.");
+        }
+    }
 }
---
+++ teoria/teoria.md
@@ -0,0 +1,18 @@
+# TEORIA PROGRAMACIÓN II
+
+## CLASES:
+- *Atributos:* características de los elementos de la clase.
+
+## MÉTODOS:
+- Acciones contenidas en una clase y define su comportamiento.
+
+- Son verbos en infinitivo que representan la acción que harán (minusculaMayuscula).
+
+- Pueden tener valores de entrada (parámetros) y valores de salida (valores de retorno).
+
+- Pueden ser Porcedimientos (no retornan valor) o Funciones (retornan unn valor).
+
+
+
+
+
